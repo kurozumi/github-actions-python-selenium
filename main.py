46cc5485bec4d78e663494fa99809f65d43bbb1a
kurozumi <info@a-zumi.net>
2021-03-10 11:35:52 +0900

    update

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get("https://v4.eccube-plugin.net/")
-assert "あずみ.net" in driver.title
+assert "workflows" in driver.title
 driver.close()
