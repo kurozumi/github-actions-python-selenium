ab4cc803c207983d2a2eec9dc1a21279f18d4eb3
kurozumi <info@a-zumi.net>
2021-03-10 14:03:14 +0900

    update

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_argument('--headless')
-driver = webdriver.Chrome(chrome_options=options)
-driver.get("https://v4.eccube-plugin.net/")
-assert "あずみ.net" in driver.title
-driver.close()
+class SeleniumTest(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument('--headless')
+        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver.get("https://v4.eccube-plugin.net/")
+
+    def tearDown(self):
+        self.driver.close()
+        self.driver.quit()
+        
+    def test_1(self):
+        assert "あずみ.net" in self.driver.title
+
+if __name__ == "__main__":
+    unittest.main()
