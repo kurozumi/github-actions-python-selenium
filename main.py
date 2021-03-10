a350d6327a24fc62b14d011c24490fa11db2bf15
kurozumi <info@a-zumi.net>
2021-03-10 13:12:02 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome(chrome_options=options)
 driver.get("https://v4.eccube-plugin.net/")
 assert "workflows" in driver.title
 driver.close()
